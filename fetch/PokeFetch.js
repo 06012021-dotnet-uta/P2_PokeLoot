9eb14a743ca667dffa37f8d71d5dc5ee4cd3484f
ChristianRevature <christian.romero@revature.net>
2021-07-06 16:01:01 -0400

    changes to yaml and updates fetches

--- fetch/PokeFetch.js
+++ fetch/PokeFetch.js
@@ -1,14 +1,23 @@
+/*
+Fetching basic card data
+Fetching "description card data"
+Creating tier list based on Base stats
+*/
+
+
+//----------------------------FETCHING BASIC CARD DATA AND INSERTING------------------------------------------------------------
 /*Business need: retrieving any pokemon's info based on some integer that is given to the function (assums the function is given an array)
 1) Create simple fetch request for a single pokemon - DONE
 2) Create simple fetech request and print out a multiple values for one pokemon -DONE
 3) Scale script to work when given an array of integers 
 4) Scale script to work when given an array of integers and return multiple fields
+5) Inject the data into front end html
 */ 
 
-//Step 1 & 2
+//STEP 1 AND 2
 var arr = [];
     
-function fetchpoke(){
+function fetchpokesprite(){
     fetch('https://pokeapi.co/api/v2/pokemon/1')//fetch req
     .then(res => {
         if(res.ok) return res.json()})                    //takes the promise & converts to JSON. return allows next .then to use the object. Added validation
@@ -17,10 +26,12 @@ function fetchpoke(){
         console.log(error)
     })
 }
-fetchpoke();
+fetchpokesprite();
 //front_default
 //front_shiny
 
+
+//STEP 3 - going up till 809 because pokeapi might have errors for sword & shield? bulbapedia shows that 809 is last pokemon before sword & shield
 
 //We can try Promise.all - maybe for 1 fetch from api & 1 from our own API, if needed in the future?
 
@@ -31,11 +42,6 @@ function promisetest(){
     .then(values =>  {return Promise.all(values.map ( r => r.json()))}) //this is mapping values from each promise into an array, making an array of arrays
     .then()//if we use values from previous, the current then will have an array of arrays (deconstruct array)
 }
-
-
-
-
-//Step 3 - going up till 809 because pokeapi might have errors for sword & shield? bulbapedia shows that 809 is last pokemon before sword & shield
 
 //create & populate array
 const arrayId = new Array(808);
@@ -85,24 +91,42 @@ fetchloop();
 
   //function to populate images
   var counter = 0;
-  var userCollection = [1,2,3];
-  document.getElementById('view-collection').addEventListener('click', changeImgs);
+  var userCollection = [123,235,489];
+  var userCollectionShiny = [100,200,300]
+  document.getElementById('view-collection').addEventListener('click', changeNormals);
+  document.getElementById('view-collection-shiny').addEventListener('click', changeShinies);
+
+
   function changeImgs() {
-    
-    {
-      for(counter=0;counter<userCollection;counter++){      //takes counter & increments 
-        document.getElementById('picture'+`${i}`).src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+`${i}`+".png";
-        document.getElementById('picture'+`${i}`).alt = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+`${i}`+".png";
-      }
+    for(counter = 0; counter < userCollection.length; counter++){   //takes counter & increments 
+      document.getElementById('picture'+`${counter}`).src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+`${userCollection[counter]}`+".png";
+      document.getElementById('picture'+`${counter}`).alt = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+`${userCollection[counter]}`+".png";
     }
-    
+  }
      //logic to distinguish between shiny & normal?
-       /*
-        function changeShiny() {
-        counter++;//maybe put an array here?
-        document.getElementById('picture').src = 'pic-' + counter + '.jpg';
-        document.getElementById('picture').alt = 'pic-' + counter + '.jpg';*/
+     function changeShiny() {
+        for(counter = 0; counter < userCollection.length; counter++){   //takes counter & increments 
+        document.getElementById('picture'+`${counter}`+'shiny').src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+`${userCollectionShiny[counter]}`+".png";
+        document.getElementById('picture'+`${counter}`+'shiny').alt = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+`${userCollectionShiny[counter]}`+".png";
+        }
+        }
+
+//these functions are going to be the CONTAINER functions for the functions that change content 
+function changeNormals(){
+    changeImgs();
 }
+function changeShinies(){
+    changeShiny();
+}
+
+
+
+
+
+
+
+
+
 
 
 
@@ -111,7 +135,6 @@ fetchloop();
 2. Configure capability to alter image sources based on values from an array (usercollection array?) - DONE
 */
 var counter=0;
-var userCollection = [3,6,9];
 var userSprites = [];
 
 for(let i = 0; i < userCollection.length;i++ ){         
@@ -120,7 +143,7 @@ for(let i = 0; i < userCollection.length;i++ ){
 
 //console.log(userSprites)
 
-for(counter = 0;counter<userCollection;counter++) {                  
+for(counter = 0; counter <= userCollection.length; counter++) {                  
     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)   
       .then(res => { if(res.ok) return res.json()})
       .then(res => console.log(res.sprites.front_default, res.sprites.front_shiny)) //don't print to console but save
@@ -134,15 +157,37 @@ for(counter = 0;counter<userCollection;counter++) {
 
   //maybe learn how to do this in angular?
 
-  //------------------------------------------------------------------------------------------------
-  /*Pokemon rarity tiers based on base stats: 5 tiers
-  SSR: Specially Super Rare (650+ BS or 'legendaries')
-  SR: Super Rare            (501-650)
-  R: Rare                   (401-500)
-  C: Common                 (325-400)
-  VC: Very Common           (150-325 BS)
 
-  Because the PokeAPI doesn't support Sword and Shield, the functional ranges of pokemon base stats is [150,800].
-  
-  Source: https://www.reddit.com/r/MandJTV/comments/fvza5t/every_single_pokemon_base_stat_tier_list_finnaly/
-  */
+
+//----------------------------FETCHING EXTRA CARD DATA------------------------------------------------------------
+/*Description data consists of:
+Base Stats
+Type
+Height
+Weight
+*/
+
+//After reviewing the API data, it seems that height & weight are in Metric system & should have a decimal point after the before the last digit 
+function fetchpokedesc(){
+    fetch('https://pokeapi.co/api/v2/pokemon/445')//fetch req
+    .then(res => {
+        if(res.ok) return res.json()})                    //takes the promise & converts to JSON. return allows next .then to use the object. Added validation
+    .then(res => console.log(res.stats, res.types, res.height.toFixed(1), typeof(res.height.toFixed(1)), 
+    parseFloat(res.height.toFixed(1)), typeof(parseFloat(res.height.toFixed(1)))))
+    //.toFixed(decimal places) converts the data to a string, but for some reason the parseFloat method doesn't convert the string to a float - only to an int
+    }
+fetchpokedesc();
+
+
+//----------------------------------CREATING A TIER LIST BASED ON STATS--------------------------------------------------------------
+/*Pokemon rarity tiers based on base stats: 5 tiers
+SSR: Specially Super Rare (650+ BS or 'legendaries')
+SR: Super Rare            (501-650)
+R: Rare                   (401-500)
+C: Common                 (325-400)
+VC: Very Common           (150-325 BS)
+
+Because the PokeAPI doesn't support Sword and Shield, the functional ranges of pokemon base stats is [150,800].
+
+Source: https://www.reddit.com/r/MandJTV/comments/fvza5t/every_single_pokemon_base_stat_tier_list_finnaly/
+*/
