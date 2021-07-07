440ed04bf3571390bc7a43a191f549ad9c07fec5
Alain-Duplan <aduplan@umass.edu>
2021-07-07 12:01:10 -0400

    populate database

---
+++ SQL/ViewTables.sql
@@ -0,0 +1,8 @@
+select * from Users;
+select * from RarityTypes
+Select * from PokemonCards;
+Select * from CardCollection;
+Select * from POsts;
+
+select * from PostTypes;
+select * from DisplayBoard;
