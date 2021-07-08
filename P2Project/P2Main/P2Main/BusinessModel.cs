a141d504cdb08968db6b1f437b25ad6cc3275641
Alain-Duplan <aduplan@umass.edu>
2021-07-08 12:33:23 -0400

    lootbox functionality

--- P2Project/P2Main/P2Main/BusinessModel.cs
+++ P2Project/P2Main/P2Main/BusinessModel.cs
@@ -9,9 +9,31 @@ namespace BusinessLayer
 
         P2DbClass context = new P2DbClass();
         
-
-        public P2DbContext.Models.PokemonCard lootbox(){
+        public P2DbContext.Models.PokemonCard rollLootbox(P2DbContext.Models.User user){
             Random random = new Random();
+            P2DbContext.Models.PokemonCard card = lootbox(random);
+            int shiny = random.Next(101);
+            CardCollection coll = context.CardCollections.Where(x => x.UserId == user.UserId && x.PokemonId == card.PokemonId).FirstOrDefault();
+            if(coll == null){
+                coll.UserId = user.UserId;
+                coll.PokemonId = card.PokemonId;
+                coll.QuantityNormal = 0;
+                coll.QuantityShiny = 0;
+                //context.CardCollections.Add(coll);
+                
+            }
+            if(shiny < 99){
+                coll.QuantityNormal++;
+            }
+            else{
+                coll.QuantityShiny++;
+                card.isShiny = true;    
+            }
+            //context.SaveChanges();
+            return card;            
+        }
+
+        private P2DbContext.Models.PokemonCard lootbox(Random random){            
             int rare = genRarity(random);
             var pokeList = context.PokemonCards.Where(x => x.RarityId == rare).ToList();
             int rand = random.Next(pokeList.Count);
