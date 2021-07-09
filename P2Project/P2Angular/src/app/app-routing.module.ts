0ae92ea4d046dc71735d9a3e412471da56a84bc5
Adrian-Gonzalez-Revature <adrian.gonzalez@revature.net>
2021-07-09 13:26:56 -0600

    Added routing

--- P2Project/P2Angular/src/app/app-routing.module.ts
+++ P2Project/P2Angular/src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
@@ -12,27 +10,25 @@ import { TradeCardPageComponent } from './trade-card-page/trade-card-page.compon
 import { UnlockCardPageComponent } from './unlock-card-page/unlock-card-page.component';
 import { ViewInformationPageComponent } from './view-information-page/view-information-page.component';
 import { ViewBalancePageComponent } from './view-balance-page/view-balance-page.component';
-import { AppRoutingModule } from './app-routing.module';
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'Login', pathMatch: 'full' },
+  { path: 'Login', component: LoginPageComponent },
+  { path: 'Signup', component: SignupPageComponent },
+  { path: 'Home/:id', component: HomePageComponent },
+  { path: 'Profile/:id', component: ProfilePageComponent },
+  { path: 'Game/:id', component: GamePageComponent },
+  { path: 'Post/:id', component: CreatePostComponent },
+  { path: 'TradeCard/:id', component: TradeCardPageComponent },
+  { path: 'UnlockCard/:id', component: UnlockCardPageComponent },
+  { path: 'ViewInformation/:id', component: ViewInformationPageComponent },
+  { path: 'ViewBalance/:id', component: ViewBalancePageComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomePageComponent,
-    LoginPageComponent,
-    SignupPageComponent,
-    ProfilePageComponent,
-    GamePageComponent,
-    CreatePostComponent,
-    TradeCardPageComponent,
-    UnlockCardPageComponent,
-    ViewInformationPageComponent,
-    ViewBalancePageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppModule { }
+export class AppRoutingModule { }
