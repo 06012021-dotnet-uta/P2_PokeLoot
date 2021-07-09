aa321da35c7d9251585ff1acc5e3ab5c3d487c32
Mason <mason.sanborn@revature.net>
2021-07-09 10:04:31 -0500

    added line in karma.config for code reporting

--- P2Project/P2Angular/karma.conf.js
+++ P2Project/P2Angular/karma.conf.js
@@ -29,7 +29,8 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: "lcov" }  // added for code reporting as mark instructed
       ]
     },
     reporters: ['progress', 'kjhtml'],
