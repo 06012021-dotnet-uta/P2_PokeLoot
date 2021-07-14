ac38b41604c8ac73ad57b45908ba94eba7f915ec
ChristianRevature <christian.romero@revature.net>
2021-07-14 15:08:25 -0400

    refines collection fucntionality

---
+++ P2Project/P2Angular/src/app/cardcollect/MockCardCollection.ts
@@ -0,0 +1,24 @@
+import { Card } from "./card"
+
+export const mockCardCollection:Card[] =[
+    {
+    pokeid:1,
+    pokename:'bulbasaur',
+    rarity:5,
+    spriteNorm:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
+    spriteShiny:"spriteshiny",
+    },
+    {
+      pokeid:4,
+      pokename:'squirtle',
+      rarity:5,
+      spriteNorm:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
+      spriteShiny:"spriteshiny",
+    },
+    {
+    pokeid:7,
+    pokename:'charmander',
+    rarity:5,
+    spriteNorm:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
+    spriteShiny:"spriteshiny"
+    }]
