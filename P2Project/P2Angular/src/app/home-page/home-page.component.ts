03555a3e674ad030cb0ccb3533cc90bdd3f50f87
Alain-Duplan <aduplan@umass.edu>
2021-07-16 18:42:08 -0400

    allow home page to display posts

--- P2Project/P2Angular/src/app/home-page/home-page.component.ts
+++ P2Project/P2Angular/src/app/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DisplayServiceService } from '../display-service.service';
+import { IPost } from '../Models/IPost';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  displayBoard: IPost[];
+  
+  constructor(private _displayService : DisplayServiceService) {
+    this.displayBoard = [];
+   }
 
+  
+   //we should edit the api to also recieve the original username of poster
   ngOnInit(): void {
+    this._displayService.DisplayBoard().subscribe(
+      result => {
+        for(let i = 0; i < result.length; i++){
+          let PostId = result[i].postId;
+          let PokemonId = result[i].pokemonId;
+          let PostTime = result[i].postTime;
+          let PostDescription = result[i].postDescription;
+          let Price = result[i].price;
+          let StillAvailable = result[i].stillAvailable;
+          let IsShiny = result[i].isShiny;
+
+          let Post: IPost = {PostId, PokemonId, PostTime, PostDescription, Price, StillAvailable, IsShiny}
+          this.displayBoard.push(Post);
+        }
+      }
+    )
   }
 
 }
