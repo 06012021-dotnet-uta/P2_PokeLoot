4e586589af00dcc5c779a4487a351ce718d27106
Mason Sanborn <85184263+msanb99@users.noreply.github.com>
2021-07-16 03:08:02 -0500

    Revert "test"
    This reverts commit 500d2cf255ac58233fecd19c78dcf0ad7dc398d6.

--- P2Project/P2Angular/src/app/cardcollect/cardcollect.component.ts
+++ P2Project/P2Angular/src/app/cardcollect/cardcollect.component.ts
@@ -1,102 +1,66 @@
+import { IcuPlaceholder } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
 import { CardServiceService } from '../card-service.service';
-import { Card } from './card'; 
-import { Dictionary } from './IDictionary';
-import { User } from './IUser';
+import { ICard } from './ICard';
+import { Observable, of, BehaviorSubject } from 'rxjs';
+
 
 @Component({
   selector: 'app-cardcollect',
   templateUrl: './cardcollect.component.html',
   styleUrls: ['./cardcollect.component.css']
 })
+
 export class CardCollectComponent implements OnInit {
-  title:string ="CardCollectComponent";
-  user:User= {
-    UserId:900,
-    FirstName:'C',
-    LastName:'string',
-    UserName:'string',
-    Password:'string',
-    Email:'string',
-    CoinBalance:100,
-    AccountLevel:100,
-    TotalCoinsEarned:100,
-};
-    userCollection:Dictionary[]=[];
-    //Deprecated - userCollection is no longer Card[] type w/ API up and runnning
-  // userCollection:Card[]=[{
-  //   pokeid:1,
-  //   pokename:'Bulbasaur',
-  //   rarity:5,
-  //   spriteNorm:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
-  //   spriteShiny:"spriteshiny",
-  //   },
-  //   {
-  //     pokeid:4,
-  //     pokename:'Squirtle',
-  //     rarity:5,
-  //     spriteNorm:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
-  //     spriteShiny:"spriteshiny",
-  //   },
-  //   {
-  //   pokeid:7,
-  //   pokename:'Charmander',
-  //   rarity:5,
-  //   spriteNorm:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
-  //   spriteShiny:"spriteshiny"
-  //   }
-  //   ];
+
+  userCollection: ICard[];
+  private userId = localStorage.getItem('userId')
 
 
-  // AddCardTest(event:Card):void{
-  //   this.service.AddsCardsTest().subscribe(x => console.log("card added"));
-  // }
+  constructor(private _cardcollectionService: CardServiceService) { 
+    this.userCollection = []
 
-  
-
-
-  constructor(private _cardcollectionService: CardServiceService ) { }
-
-  ngOnInit():void {
-    this._cardcollectionService.GetCardsList().subscribe(
-      x => {this.userCollection = x},
-      y => {
-
-      }
-    );
   }
 
-  //Method parse the service return:
+  ngOnInit(): void {
+    if(this.userId != null)
+    {
+      this._cardcollectionService.GetCardsList(this.userId).subscribe(
+        result => {
+          for(let i = 0; i < result.length; i++)
+          {
+            
+            let PokemonId     = result[i].Key.PokemonId;
+            let Amount        = result[i].Key.QuantityNormal;
+            let AmountShiny   = result[i].Key.QuantityShiny;
+            let RarityId      = result[i].Value.RarityId;
+            let Link          = result[i].Value.SpriteLink;
+            let LinkShiny     = result[i].Value.SpriteLinkShiny;
+            let PokemonName   = result[i].Value.PokemonName;
 
-  //Method to display user's card collection data:
+            if(Amount > 0)
+            {
+              let Quantity = Amount;
+              let SpriteLink = Link;
+              let IsShiny = false;
+              let card : ICard = {PokemonId, Quantity, RarityId, SpriteLink, PokemonName, IsShiny};
+              this.userCollection.push(card);
+            }
+            if(AmountShiny > 0)
+            {
+              let Quantity = AmountShiny;
+              let SpriteLink = LinkShiny;
+              let IsShiny = true;
+              let card : ICard = {PokemonId, Quantity, RarityId, SpriteLink, PokemonName, IsShiny};
+              this.userCollection.push(card);
+            }
+            
+          }
 
-  //Method to sort collection based on ID
+        }
 
-  //Method to sort collection based on Rarity
+      );
+    }
 
-  //Method to sort collection based on Name
-
-
-
-
-
-/*Notes: Creating a form
-Make sure ti import FOrmsModule in app.module
-NameForm:FormGroup = new FormGroup({
-  propertyname: new FormControl(default_value, arrayValidators=[Validators.Validators_property,...,Validators.Validators_property]),
-  ...
-  propertyname: new FormControl(default_value, arrayValidators=[Validators.Validators_property,...,Validators.Validators_property]),
-
-  2) Then create a form in html that is [] bound to your NameForm and also eventbound to (ngSubmit)="SubmitFunction($event)"
-  <label for 
-  <input type= ; name = ; placeholder = ; formControlName = "property-nameK"; class = "form-control">
-})
-*/
-
-
-/*This is our callback funciton for the event, when making form submission
-NameFormSubmit(event:MousEvent/submitevent):void{
-
-}
-*/ 
+  }
 }
