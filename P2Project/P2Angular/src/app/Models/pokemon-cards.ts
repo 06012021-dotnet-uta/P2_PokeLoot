78236bb05b4a410bd0f7a7d7776d98bf990021dd
Alain-Duplan <aduplan@umass.edu>
2021-07-16 04:14:25 -0400

    formatted displaying new card

--- P2Project/P2Angular/src/app/Models/pokemon-cards.ts
+++ P2Project/P2Angular/src/app/Models/pokemon-cards.ts
@@ -1,2 +1,9 @@
 export interface PokemonCards {
+    PokemonId:number,
+    PokemonName:string,
+    RarityID:number,
+    IsShiny:boolean,
+    SpriteLink:string,
+    SpriteLinkShiny:string,
+    MainSprite:string,
 }
--- P2Project/P2Angular/src/app/add-card/add-card.component.ts
+++ P2Project/P2Angular/src/app/add-card/add-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Card } from '../cardcollect/card';
+import { PokemonCards } from '../Models/pokemon-cards';
 
 @Component({
   selector: 'app-add-card',
@@ -8,12 +9,28 @@ import { Card } from '../cardcollect/card';
 })
 export class AddCardComponent implements OnInit {
 
-  //newCard:Card =
+  newCard:PokemonCards  = {
+    PokemonId: 150,
+    PokemonName: 'mewtwo',
+    RarityID: 5,
+    IsShiny: false,
+    SpriteLink:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png',
+    SpriteLinkShiny: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/150.png',
+    MainSprite: '',
+  };
+  
   constructor() { }
   
   //method to update array in usercollection
   
   ngOnInit(): void {
+    if (this.newCard.IsShiny == true) {
+      this.newCard.MainSprite = this.newCard.SpriteLink;
+    } else {
+      this.newCard.MainSprite = this.newCard.SpriteLinkShiny;
+    }
+    
   }
 
+  
 }
