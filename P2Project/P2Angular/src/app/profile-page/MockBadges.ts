58565a9ec073b2aa53b8defcb991d029fa858d7d
ChristianRevature <christian.romero@revature.net>
2021-07-18 12:20:41 -0400

    updates package.json + profile page test

---
+++ P2Project/P2Angular/src/app/profile-page/MockBadges.ts
@@ -0,0 +1,26 @@
+import { Badge } from "./IBadge";
+
+export const MockBadges:Badge[] =[
+    {Title:'hello',
+    Completed:false},
+    {
+        Title:'world',
+        Completed:false
+    },
+    {
+        Title:'this',
+        Completed:false
+    },
+    {
+        Title:'is',
+        Completed:false
+    },
+    {
+        Title:'a',
+        Completed:false
+    },
+    {
+        Title:'test',
+        Completed:false
+    }
+];
