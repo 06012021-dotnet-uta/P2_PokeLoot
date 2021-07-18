8c4d5a31d8c8c0a9062c5c245cbded610a022fe3
Mason Sanborn <85184263+msanb99@users.noreply.github.com>
2021-07-18 15:38:40 -0500

    removed comments for extra lines and confirmed lootbox properly decrements coins

--- P2Project/P2Angular/src/app/unlock-card-service.ts
+++ P2Project/P2Angular/src/app/unlock-card-service.ts
@@ -20,10 +20,6 @@ export class UnlockCardService {
     return this.http.get<any>('https://pokeloot.azurewebsites.net/api/P2/Lootbox/' + localStorage.getItem('userId'))
   }
 
-  // GetBalance():Observable<any[]>{
-  //   return this.http.get<any>('https://pokeloot.azurewebsites.net/api/P2/CoinBalance/' + localStorage.getItem('userId'))
-  // }
-
   Balance():Observable<any[]>{
     return this.http.get<any>('https://pokeloot.azurewebsites.net/api/P2/Balance/' + localStorage.getItem('userId'))
   }
