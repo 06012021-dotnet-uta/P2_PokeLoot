c57aefa04dae951d05fc8578c1399d85aee31b67
Adrian-Gonzalez-Revature <adrian.gonzalez@revature.net>
2021-07-19 00:10:21 -0600

    Create Post function works

--- P2Project/P2Angular/src/app/create-post-page/create-post.component.ts
+++ P2Project/P2Angular/src/app/create-post-page/create-post.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { CardServiceService } from '../card-service.service';
 import { ICard } from '../cardcollect/ICard';
 import { IPost } from '../Models/IPost';
+import { FullPost } from '../Models/Post';
 import { CreatePostService } from '../service/createPost/create-post.service';
 
 
@@ -15,13 +16,17 @@ export class CreatePostComponent implements OnInit {
 
   isPriceValid: boolean = false;
   isPostTypeSelect = false;
+  isCardChosen = false;
+  isDescription = false;
+  isPokemonId = false;
   submittedSuccesfully = false;
   private userId = localStorage.getItem('userId') as string;
   user = localStorage.getItem('user') as string
   userName!: string;
   bublapedia: string = 'https://bulbapedia.bulbagarden.net/wiki/';
   userCollection: ICard[];
-  postType = [{ type: "Display" }, { type: "Sale" }, { type: "Discussion" }];
+  postType = [{ type: "Display" }, { type: "Sale" }];
+
 
 
   constructor(private _cardcollectionService: CardServiceService, private _createPostService: CreatePostService) {
@@ -71,6 +76,10 @@ export class CreatePostComponent implements OnInit {
   OnSubmit(postForm: NgForm) {
     this.isPriceValid = false;
     this.isPostTypeSelect = false;
+    this.isPokemonId = false;
+    this.isDescription = false;
+
+
 
     if (postForm.value.postType === 'Sale') {
       this.isPriceValid
@@ -82,6 +91,16 @@ export class CreatePostComponent implements OnInit {
 
     if (postForm.value.postType === "") {
       this.isPostTypeSelect = true;
+      return;
+    }
+
+    if (postForm.value.textDescription === "") {
+      this.isDescription = true;
+      return;
+    }
+
+    if (postForm.value.PokemonId.length == 0) {
+      this.isPokemonId = true;
       return;
     }
 
@@ -97,19 +116,22 @@ export class CreatePostComponent implements OnInit {
       }
     }
 
-    let post = {
-      PokemonId: card.PokemonId,
-      PostDescription: postForm.value.textDescription,
-      Price: postForm.value.postType === 'Sale' ? postForm.value.Price : 0,
-      StillAvailable: postForm.value.postType === 'Sale' ? true : false,
-      IsShiny: card.IsShiny,
-      UserId: parseInt(this.userId),
-      UserName: this.userName,
-      SpriteLink: card.SpriteLink,
-      PostType: postForm.value.postType,
-      PokemonName: card.PokemonName,
-      RarityId: card.RarityId,
+    let post: FullPost = {
+      pokemonId: card.PokemonId,
+      postTime: new Date(),
+      postDescription: postForm.value.textDescription,
+      price: postForm.value.postType === 'Sale' ? postForm.value.Price : 0,
+      stillAvailable: postForm.value.postType === 'Sale' ? true : false,
+      isShiny: card.IsShiny,
+      userId: parseInt(this.userId),
+      userName: this.userName,
+      spriteLink: card.SpriteLink,
+      postType: postForm.value.postType,
+      pokemonName: card.PokemonName,
+      rarityId: card.RarityId,
     }
+
+    console.log(post);
 
     this._createPostService.CreatePost(post).subscribe(
       result => {
--- P2Project/P2Angular/src/app/service/createPost/create-post.service.ts
+++ P2Project/P2Angular/src/app/service/createPost/create-post.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { IPost } from 'src/app/Models/IPost';
+import { FullPost } from 'src/app/Models/Post';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,15 @@ import { Router } from '@angular/router';
 export class CreatePostService {
 
 
-  private url: string = "https://localhost:44307/api/P2/Post/Create"
+  private url: string = "https://localhost:44307/api/P2/Post"
 
   constructor(private router: Router, private http: HttpClient) { }
 
-  CreatePost(newPost: any) {
-    return this.http.post<any>(this.url, newPost)
+  CreatePost(newPost: FullPost) {
+    var post = JSON.stringify(newPost);
+    return this.http.get<any>(this.url + `/${newPost.pokemonId}` + `/${newPost.price}` + `/${newPost.isShiny}` + `/${newPost.userId}` + `/${newPost.postDescription}`);
   }
 }
+
+
+"​/api​/P2​/Post​/{pokemonId}​/{postDecription}​/{postPrice}​/{isShiny}​/{userId}"
